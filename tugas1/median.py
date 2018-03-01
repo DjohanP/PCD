e48b3ceeac9c57dc2c6f89de6d46d44e7216c774
djohanp <djohan.prabowo1927@gmail.com>
2018-03-02 00:36:05 +0700

    Tugas 1

--- tugas1/median.py
+++ tugas1/median.py
@@ -4,21 +4,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-img=cv2.imread('856876.fig.001b.jpg',0)
+img=cv2.imread('zebra.jpg',0)
 img2=img.copy()
 height, width = img.shape[:2]
 
-for i in np.arange(3, height-3):
-    for j in np.arange(3, width-3):
+for i in np.arange(1, height-1):
+    for j in np.arange(1, width-1):
         neighbors = []
-        for k in np.arange(-3, 4):
-            for l in np.arange(-3, 4):
+        for k in np.arange(-1, 2):
+            for l in np.arange(-1, 2):
                 a = img.item(i+k, j+l)
                 neighbors.append(a)
         neighbors.sort()
-        median = neighbors[24]
+        median = neighbors[4]
         b = median
         img2.itemset((i,j), b)
+cv2.imwrite("zebramedian.jpg",img2)
 print "oke"
 images = [img,img2]
 titles = ['Original Image','Median Image']
